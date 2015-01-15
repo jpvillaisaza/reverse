16dad87a47e1c01d9abd935559c1c8b6b036cd5a
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2015-01-15 18:00:18 -0500

    Add some examples and properties

--- src/Reverse/Data/List.hs
+++ src/Reverse/Data/List.hs
@@ -1,26 +1,53 @@
-module Reverse.Data.List where
+------------------------------------------------------------------------------
+-- |
+-- Module      : Reverse.Data.List
+--
+--
+--
+------------------------------------------------------------------------------
 
-import Prelude ((++))
+module Reverse.Data.List
+  (obverse
+  ,reverse)
+  where
 
--- reverse [1..10000] 3.12 3.07
+import Prelude hiding (reverse)
+
+-- |
+--
+-- prop> [] ++ xs == xs
+-- prop> xs ++ [] == xs
+--
+-- prop> xs ++ ys ++ zs == (xs ++ ys) ++ zs
+
+-- |
+--
+-- Examples:
+--
+-- >>> obverse [9,8..0]
+-- [0,1,2,3,4,5,6,7,8,9]
+--
+-- Properties:
+--
+-- prop> xs == (obverse . obverse) xs
+
+obverse :: [a] -> [a]
+obverse []     = []
+obverse (x:xs) = obverse xs ++ [x]
+
+-- |
+--
+-- Examples:
+--
+-- >>> reverse [0..9]
+-- [9,8,7,6,5,4,3,2,1,0]
+--
+-- Properties:
+--
+-- prop> xs == (reverse . reverse) (xs :: [Int])
 
 reverse :: [a] -> [a]
-reverse []     = []
-reverse (x:xs) = reverse xs ++ [x]
-
--- 0.22 0.22
-
-reverse' :: [a] -> [a]
-reverse' xs = reverse'' xs []
+reverse xs = reverse' xs []
   where
-    reverse'' []     ys = ys
-    reverse'' (x:xs) ys = reverse'' xs (x:ys)
-
--- 0.23 0.23
-
-reverse'' :: [a] -> [a]
-reverse'' xs = reverse''' xs []
-
-reverse''' :: [a] -> [a] -> [a]
-reverse''' []     ys = ys
-reverse''' (x:xs) ys = reverse''' xs (x:ys)
+    reverse' []     ys = ys
+    reverse' (x:xs) ys = reverse' xs (x:ys)
