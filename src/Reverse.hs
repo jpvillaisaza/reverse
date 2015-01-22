4016ad7b884a07bc9a518abcbcf517fd7a3de5f0
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2015-01-22 17:24:54 -0500

    Add export list

--- src/Reverse.hs
+++ src/Reverse.hs
@@ -1,1 +1,10 @@
-module Reverse where
+module Reverse
+  (clockSomething
+  ,obverse
+  ,reverse)
+  where
+
+import Reverse.Data.List (obverse,reverse)
+import Reverse.System.Clock (clockSomething)
+
+import Prelude hiding (reverse)
