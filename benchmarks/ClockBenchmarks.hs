1d64391794c65aa1637815662e60cece46396c40
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2015-06-04 10:22:02 -0500

    Modify clock benchmarks

--- benchmarks/ClockBenchmarks.hs
+++ benchmarks/ClockBenchmarks.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
 
--- |
+-- | Obverse versus Reverse: clock benchmarks.
 
 module Main (main) where
 
@@ -10,16 +10,19 @@ import Reverse (obverse,reverse)
 import Control.Exception (evaluate)
 import Control.Monad (void)
 import Data.Time (diffUTCTime,getCurrentTime)
-import Formatting (fprint,(%))
+import Formatting ((%),fprint)
 import Formatting.Clock (timeSpecs)
 import Prelude hiding (reverse)
 import System.Clock (Clock(Monotonic),getTime)
+import Test.QuickCheck (Arbitrary(arbitrary),Gen,generate,vectorOf)
 
 -- |
 --
+-- Clock something and print the result.
+--
 -- Example:
 --
--- >>> clockSomething (obverse [1..1000])
+-- >>> clockSomething (reverse [1..1000])
 --
 -- See <http://chrisdone.com/posts/measuring-duration-in-haskell>.
 
@@ -30,6 +33,14 @@ clockSomething something = do
   end <- getTime Monotonic
   fprint (timeSpecs % "\n") start end
 
+-- |
+--
+-- Time something and print the result.
+--
+-- Example:
+--
+-- >>> timeSomething (reverse [1..1000])
+
 timeSomething :: a -> IO ()
 timeSomething something = do
   start <- getCurrentTime
@@ -37,7 +48,33 @@ timeSomething something = do
   end <- getCurrentTime
   print (diffUTCTime end start)
 
+-- |
+--
+-- Generate a list of a given length of arbitrary integers.
+
+arbitraryIntVectorOf :: Int -> IO [Int]
+arbitraryIntVectorOf n = generate (vectorOf n arbitrary)
+
 main :: IO ()
 main = do
-  clockSomething (obverse [1..10000000])
-  clockSomething (reverse [1..10000000])
+  putStrLn "obverse versus reverse/10000000..."
+  putStr   "  obverse/1... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . obverse
+  putStr   "  reverse/1... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . reverse
+  putStr   "  obverse/2... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . obverse
+  putStr   "  reverse/2... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . reverse
+  putStr   "  obverse/3... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . obverse
+  putStr   "  reverse/3... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . reverse
+  putStr   "  obverse/4... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . obverse
+  putStr   "  reverse/4... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . reverse
+  putStr   "  obverse/5... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . obverse
+  putStr   "  reverse/5... "
+  arbitraryIntVectorOf 10000000 >>= clockSomething . reverse
